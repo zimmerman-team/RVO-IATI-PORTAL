f8aff9172d94a249a264355f2c76f1963c1e69d3
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2016-09-20 10:20:56 +0200

    last updated update

--- javascripts/_layout/controller.index.js
+++ javascripts/_layout/controller.index.js
@@ -9,13 +9,16 @@
     .module('oipa.layout')
     .controller('IndexController', IndexController);
 
-  IndexController.$inject = ['$scope', '$sce', 'FilterSelection', 'Aggregations', 'TransactionAggregations', 'Activities'];
+  IndexController.$inject = ['$scope', '$sce', 'FilterSelection', 'Aggregations', 'TransactionAggregations', 'Activities', '$http', '$filter'];
 
-  function IndexController($scope, $sce, FilterSelection, Aggregations, TransactionAggregations, Activities) {
+  function IndexController($scope, $sce, FilterSelection, Aggregations, TransactionAggregations, Activities, $http, $filter) {
     var vm = this;
     vm.countryCount = null;
     vm.activityCount = null;
     vm.budget = null;
+    vm.date_updated = '';
+
+
     activate();
 
     function activate() {
@@ -35,6 +38,18 @@
       TransactionAggregations.aggregation('reporting_organisation', 'incoming_fund', '&hierarchy=2').then(function(data, status, headers, config){
         vm.budget = data.data.results[0].incoming_fund;
       }, errorFn);
+
+      $http.get("https://iatiregistry.org/api/3/action/package_show?id=rvo-01", { cache: true }).then(function(data, status, headers, config){
+        var extras = data.data.result.extras
+        for (var i = 0; i < extras.length;i++){
+          if (extras[i].key == 'data_updated'){
+            vm.date_updated = $filter('date')(new Date(extras[i].value),'dd-MM-yyyy');
+            break;
+          }
+        }
+      }, errorFn);
+
+      
 
       function errorFn(data,status,heders,config){
         
