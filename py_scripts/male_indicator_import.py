5503c893fe49200a665f5dc9ba16432b93951e9b
Vincent van 't Westende <vincentvantwestende@hotmail.com>
2017-03-12 19:09:18 +0100

    male indicator update

--- py_scripts/male_indicator_import.py
+++ py_scripts/male_indicator_import.py
@@ -5,74 +5,70 @@ for ri in ResultIndicator.objects.filter(resultindicatortitle__narratives__conte
         result__activity__id=ri.result.activity.id, 
         resultindicatortitle__narratives__content="Number of full-time (equivalent) direct jobs supported - Total")
     #
-    total_actual_value = 0
-    for p in total.resultindicatorperiod_set.all():
-        # TODO: chage to most recent period
-        if p.actual:
-            total_actual_value += p.actual
-    #
-    if total_actual_value == 0:
-        continue
-    #
-    female_actual_value = 0
-    female_rip = None
-    for p in ri.resultindicatorperiod_set.all():
-        # TODO: chage to most recent period
-        female_rip = p
-        if p.actual:
-            female_actual_value += p.actual
-    #
-    male_value = total_actual_value - female_actual_value
-    #
-    if male_value < 1:
-        continue
-    #
-    # check if male indicator already exists, if so and not same value, update, else add
     male = ResultIndicator.objects.get(
         result__activity__id=ri.result.activity.id, 
         resultindicatortitle__narratives__content="Number of full-time (equivalent) direct jobs supported - Male")
-    male_actual_value = 0
-    for p in male.resultindicatorperiod_set.all():
-        # TODO: chage to most recent period
-        if p.actual:
-            total_actual_value += p.actual
     #
-    if total_actual_value == 0:
-        continue
-
-
-
-
-
-
-
-    ri_new = ResultIndicator(
-        result=ri.result,
-        measure=ri.measure,
-    )
-    ri_new.save()
+    years = [2015, 2016]
     #
-    print ri_new
-    ri_new_title = ResultIndicatorTitle(
-        result_indicator=ri_new,
-        primary_name="Number of full-time (equivalent) direct jobs supported - Male"
-    )
-    ri_new_title.save()
-    #
-    language = Language.objects.get(pk='en')
-    narrative = Narrative(
-        language=language,
-        content="Number of full-time (equivalent) direct jobs supported - Male",
-        related_object=ri_new_title,
-        activity=ri.result.activity
-    )
-    narrative.save()
-    # add ResultIndicatorPeriod
-    rip = ResultIndicatorPeriod(
-        result_indicator=ri_new,
-        period_start=female_rip.period_start,
-        period_end=female_rip.period_end,
-        target=None,
-        actual=male_value
-    )
-    rip.save()
+    for curyear in years:
+        #
+        total_actual_value = 0
+        female_actual_value = 0
+        male_actual_value = 0
+        female_rip = None
+        male_rip = None
+        #
+        #
+        for p in total.resultindicatorperiod_set.filter(period_end__year=years[curyear]):
+            if p.actual:
+                total_actual_value = p.actual
+        #
+        if total_actual_value == 0:
+            continue
+        #
+        #
+        for p in ri.resultindicatorperiod_set.filter(period_end__year=years[curyear]):
+            female_rip = p
+            if p.actual:
+                female_actual_value = p.actual
+        #
+        #
+        male_value = total_actual_value - female_actual_value
+        if male_value < 1:
+            print 'male value below zero, should not happen too often'
+        #
+        # if male period already exists for this year, then update. Else create new resultindicator
+        if male:
+            if male.resultindicatorperiod_set.get(period_end__year=years[curyear]).exists():
+                male.resultindicatorperiod_set.get(period_end__year=years[curyear]).update(actual=male_value)
+        else:
+            ri_new = ResultIndicator(
+                result=ri.result,
+                measure=ri.measure,
+            )
+            ri_new.save()
+            #
+            ri_new_title = ResultIndicatorTitle(
+                result_indicator=ri_new,
+                primary_name="Number of full-time (equivalent) direct jobs supported - Male"
+            )
+            ri_new_title.save()
+            #
+            language = Language.objects.get(pk='en')
+            narrative = Narrative(
+                language=language,
+                content="Number of full-time (equivalent) direct jobs supported - Male",
+                related_object=ri_new_title,
+                activity=ri.result.activity
+            )
+            narrative.save()
+            # add ResultIndicatorPeriod
+            rip = ResultIndicatorPeriod(
+                result_indicator=ri_new,
+                period_start=female_rip.period_start,
+                period_end=female_rip.period_end,
+                target=None,
+                actual=male_value
+            )
+            rip.save()
