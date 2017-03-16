6fa2c0c80800699ee6a63d15ad3e72f12c4257b0
Bryan Haakman <bryanhaakman@gmail.com>
2017-03-16 14:01:26 +0200

    Update oipa.constants.js

--- javascripts/oipa.constants.js
+++ javascripts/oipa.constants.js
@@ -25,8 +25,8 @@
       "NL-KVK-27378529-IM201601G": "Partners for Water",
       "NL-KVK-27378529-27416": "Water Program Myanmar",
       "NL-KVK-27378529-23583": "Jakarta Coastal Defence Strategy",
-      "NL-KVK-27378529-GACC160005": "Clean Cooking Sector Support"
-       "NL-KVK-27378529-27803": "Develop2Build"
+      "NL-KVK-27378529-GACC160005": "Clean Cooking Sector Support",
+      "NL-KVK-27378529-27803": "Develop2Build"
 
     }
 
@@ -50,8 +50,8 @@
       "NL-KVK-27378529-IM201601G": "PvW",
       "NL-KVK-27378529-27416": "PvW Myanmar",
       "NL-KVK-27378529-23583": "JCDS",
-      "NL-KVK-27378529-GACC160005": "CCSS"
-	 "NL-KVK-27378529-278035": "D2B"
+      "NL-KVK-27378529-GACC160005": "CCSS",
+      "NL-KVK-27378529-278035": "D2B"
     }
 
     var sdgGoals = {"1":"No poverty","2":"Zero hunger","3":"Good health and well-being","4":"Quality education","5":"Gender equality","6":"Clean water and sanitation","7":"Affordable and clean energy","8":"Decent work and economic growth","9":"Industry, innovation and infrastructure","10":"Reduced inequalities","11":"Sustainable cities and communities","12":"Responsible consumption and production","13":"Climate action","14":"Life below water","15":"Life on land","16":"Peace, justice and strong institutions","17":"Partnerships for the goals"}
