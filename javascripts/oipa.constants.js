f2d5d1a8326634432a88e699356585af6dfa6211
Stefanos <hadjipetrou.stephanos@hotmail.com>
2018-02-27 11:22:29 +0200

    Add new programmes
    SD-91

--- javascripts/oipa.constants.js
+++ javascripts/oipa.constants.js
@@ -32,7 +32,11 @@
       "NL-KVK-27378529-GACC160005": "Clean Cooking Sector Support",
       "NL-KVK-27378529-27803": "Develop2Build",
       'NL-KVK-27378529-29299':'European Partnership for Responsible Minerals (EPRM)',
-      "NL-KVK-27378529-29557": "Fund against Child Labour (FBK)"
+      "NL-KVK-27378529-29557": "Fund against Child Labour (FBK)",
+      "NL-KVK-27378529-27230": "Knowledge Fund Jakarta (KFJ)",
+      "NL-KVK-27378529-29715": "Embassy of the Kingdom of the Netherlands in Beira, Mozambique (EKN)",
+      "NL-KVK-27378529-4000000171": "Energy Transition Facility (ETF)",
+      "NL-KVK-27378529-26663TA": "Dutch Good Growth Fund – Technical Assistance (DGGF-TA)"
     }
 
     var programmaAfkortingen = {
@@ -58,7 +62,11 @@
       "NL-KVK-27378529-GACC160005": "CCSS",
       "NL-KVK-27378529-278035": "D2B",
       "NL-KVK-27378529-29299": "EPRM",
-      "NL-KVK-27378529-29557": "FBK"
+      "NL-KVK-27378529-29557": "FBK",
+      "NL-KVK-27378529-27230": "KFJ",
+      "NL-KVK-27378529-29715": "EKN",
+      "NL-KVK-27378529-4000000171": "ETF",
+      "NL-KVK-27378529-26663TA": "DGGF-TA"
     }
 
     var sdgGoals = {"1":"No poverty","2":"Zero hunger","3":"Good health and well-being","4":"Quality education","5":"Gender equality","6":"Clean water and sanitation","7":"Affordable and clean energy","8":"Decent work and economic growth","9":"Industry, innovation and infrastructure","10":"Reduced inequalities","11":"Sustainable cities and communities","12":"Responsible consumption and production","13":"Climate action","14":"Life below water","15":"Life on land","16":"Peace, justice and strong institutions","17":"Partnerships for the goals"}
