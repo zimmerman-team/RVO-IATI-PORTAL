0280bb87c368e566a52fb45786ace1ff8d5b788c
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-10-03 15:49:45 +0300

    Remove GA script

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
         <li class=""><a href="http://english.rvo.nl/home/about-rvonl/what-is-rvonl/contact-rvonl" class="pointer">Contact</a></li>
         <li class=""><a href="http://english.rvo.nl/home/about-rvonl/what-is-rvonl/disclaimer" class="pointer">Disclaimer</a></li>
       </ul>
-    </div>  
+    </div>
     <div class="col-sm-6 col-md-3">
       <h2>Topics</h2>
       <ul class="">
@@ -18,8 +18,8 @@
         <li class=""><a href="http://english.rvo.nl/topics/sustainable-entrepreneurship" class="pointer">Sustainable enterprise</a>  </li>
         <li class=""><a href="http://english.rvo.nl/topics/innovation" class="pointer">Innovative enterprise</a>  </li>
         <li class=""><a href="http://english.rvo.nl/topics" class="more"><b>All topics</b></a></li>
-      </ul>  
-    </div>  
+      </ul>
+    </div>
     <div class="col-sm-6 col-md-3">
       <h2>Related links</h2>
       <ul class="">
@@ -33,30 +33,22 @@
       <ul class="">
         <li><a href="http://english.rvo.nl/subscribe">Subscribe to our newsletter</a></li>
       </ul>
-    </div>  
-  </div> 
+    </div>
+  </div>
   <div class="row">
     <div class="col-sm-12 social">
       <a href="http://twitter.com/RVO_Nederland" title="Twitter"><i class="fa fa-twitter-square"></i></a>
-      <a href="http://www.facebook.com/rijksdienstvoorondernemendnederland" title="Facebook"><i class="fa fa-facebook-square"></i></a>  
+      <a href="http://www.facebook.com/rijksdienstvoorondernemendnederland" title="Facebook"><i class="fa fa-facebook-square"></i></a>
       <a href="http://www.linkedin.com/company/rijksdienstvoorondernemendnederland" title="Linkedin"><i class="fa fa-linkedin-square"></i></a>
       <a href="http://www.rvo.nl/rss" title="RSS"><i class="fa fa-rss-square"></i></a>
     </div>
-  </div>  
+  </div>
 </footer>
 </div>
 
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-366594-13', 'auto');
-      ga('send', 'pageview');
-    </script>
 	</body>
 </html>
