484a2d9b4989481bdcdca3a40d5c87ed7bd54d5a
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-11-06 18:37:12 +0200

    Add missing programme codes

--- javascripts/oipa.constants.js
+++ javascripts/oipa.constants.js
@@ -113,6 +113,8 @@
       "NL-KVK-27378529-4000001360": "SDGP",
       "NL-KVK-27378529-26844": "DGGF-TA",
       "NL-KVK-27378529-27803": "D2B"
+      "NL-KVK-27378529-4000002159": "DSS2",
+      "NL-KVK-27378529-4000000768": "DRR2"
     }
 
     var sdgGoals = {"1":"No poverty","2":"Zero hunger","3":"Good health and well-being","4":"Quality education","5":"Gender equality","6":"Clean water and sanitation","7":"Affordable and clean energy","8":"Decent work and economic growth","9":"Industry, innovation and infrastructure","10":"Reduced inequalities","11":"Sustainable cities and communities","12":"Responsible consumption and production","13":"Climate action","14":"Life below water","15":"Life on land","16":"Peace, justice and strong institutions","17":"Partnerships for the goals"}
